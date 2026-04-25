c3f5bc17b5a6b24952d20a14ff93038dd2b16da7
Juan Pablo Gil Félix <141673798+A01741619@users.noreply.github.com>
2026-04-24 20:55:50 -0600

    GIL - TAREA: TEST LOGIN Y CREACION DE TAREA EN TELEGRAM

--- Tests/pages/telegram/Selectors.py
+++ Tests/pages/telegram/Selectors.py
@@ -1,8 +1,18 @@
-# Selectors to be used in the Telegram page
-
+# Selectors for Telegram Web (web.telegram.org/k/)
 from selenium.webdriver.common.by import By
 
-CHAT_INPUT = (By.ID, "editable-message-text")
-SEND_BUTTON = (By.CSS_SELECTOR, ".Button.send")
-BOT_MESSAGE = (By.CSS_SELECTOR, ".message-content.text.peer-color-6")
-USER_MESSAGE = (By.CSS_SELECTOR, ".message-content.text.has-solid-background")
+CHAT_INPUT   = (By.CSS_SELECTOR,
+    "div.input-message-input[contenteditable='true'], "
+    "#editable-message-text, "
+    "div[contenteditable='true']"
+)
+BOT_MESSAGE  = (By.CSS_SELECTOR,
+    ".bubble:not(.is-out) .translatable-message, "
+    ".bubble:not(.is-out) .message, "
+    ".message-content.text.peer-color-6"
+)
+KEYBOARD_BUTTON = (By.CSS_SELECTOR,
+    ".reply-keyboard button, "
+    ".keyboard-button, "
+    "button.btn-primary.reply-keyboard-button"
+)
